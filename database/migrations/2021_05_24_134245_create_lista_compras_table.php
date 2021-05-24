4804d92009c8e5a7827a0bb44dd0c0d76983f8c8
jose1060 <josean-1060@hotmail.com>
2021-05-24 09:21:34 -0500

    Cambios por Jose

--- database/migrations/2021_05_24_134245_create_lista_compras_table.php
+++ database/migrations/2021_05_24_134245_create_lista_compras_table.php
@@ -16,6 +16,11 @@ class CreateListaComprasTable extends Migration
         Schema::create('lista_compras', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->unsignedBigInteger('producto_ID');
+            $table->unsignedBigInteger('compra_ID');
+            $table->foreign('producto_ID')->references('id')->on('productos');
+            $table->foreign('compra_ID')->references('id')->on('compras');
+            $table->integer('cantidad', 100);
         });
     }
 
