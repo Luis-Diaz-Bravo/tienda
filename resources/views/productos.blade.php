35262e2bf05c2b5089ad85e95a1609c09705cd29
jose1060 <josean-1060@hotmail.com>
2021-05-31 09:27:45 -0500

    Signed-off-by: jose1060 <josean-1060@hotmail.com>

---
+++ resources/views/productos.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <table class="table">
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Descripcion</th>
+        <th scope="col">Precio/Unidad</th>
+        <th scope="col">extistencia</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($productos as $producto)
+        <tr>
+            <th>{{$producto -> nombre}}</th>
+            <th>{{$producto -> descripcion}}</th>
+            <th>{{$producto -> precio_unidad}}</th>
+            <th>{{$producto -> existencia}}</th>
+        </tr>
+        
+        @endforeach
+    </tbody>
+    </table>
+
+
+
+@endsection
